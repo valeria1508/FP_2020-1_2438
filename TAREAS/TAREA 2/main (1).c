cadacfc2fca0d7c5eab1abec0a290d214dd37405
valeria1508 <valepadilla1508@gmail.com>
2019-10-23 17:54:46 -0700

    tarea 2

---
+++ TAREAS/TAREA 2/main (1).c
@@ -0,0 +1,11 @@
+#include <conio.h>
+#include <stdio.h>
+int main()
+{
+    int numero;
+    printf("\n Introduzca numero entero: ");
+    scanf("%d", &numero);
+    getch ()
+    return 0;
+    
+}
